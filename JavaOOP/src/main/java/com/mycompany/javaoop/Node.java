d38448ce74c15e7d468daaea8240a2f41c94ecdf
Dave <dawitabera64@gmail.com>
2019-08-30 21:58:09 -0700

    node class added, node tests added

---
+++ JavaOOP/src/main/java/com/mycompany/javaoop/Node.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.javaoop;
+
+/**
+ *
+ * @author dave
+ */
+class Node {
+    // reference to the next node in the chain, or null if there isn;t one
+    Node next;
+    // data the node can carry. could be type of any
+    Object data;
+    
+    // Node constructor
+    public Node(Object value) {
+        data = value;
+        next = null;
+    }
+    @SuppressWarnings("unused")
+    public Node(Object dataValue, Node nextValue){
+        next = nextValue; 
+        data = dataValue;
+    }
+    
+    // getData
+    public Object getDataObject(){
+        return data;
+    }
+    // setData
+    @SuppressWarnings("unused")
+    public void setData(Object dataValue){
+        data = dataValue;
+    }
+    // get the next node
+    public Node getNode(){
+        return next;
+    }
+    // set node
+    public void setNode(Node nextNode){
+        next = nextNode;
+    }
+    
+}
---
+++ JavaOOP/src/test/java/com/mycompany/javaoop/NodeTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.javaoop;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author dave
+ */
+public class NodeTest {
+    
+    public NodeTest() {
+    }
+
+    /**
+     * Test of getDataObject method, of class Node.
+     */
+    @Test
+    public void testGetDataObject() {
+    }
+
+    /**
+     * Test of setData method, of class Node.
+     */
+    @Test
+    public void testSetData() {
+    }
+
+    /**
+     * Test of getNode method, of class Node.
+     */
+    @Test
+    public void testGetNode() {
+    }
+
+    /**
+     * Test of setNode method, of class Node.
+     */
+    @Test
+    public void testSetNode() {
+    }
+    
+}
