a9a878fc522673a686a31e4cfd7f9f75bc7d717d
Dave <dawitabera64@gmail.com>
2019-08-30 22:12:19 -0700

    added method to add data to the list

--- JavaOOP/src/main/java/com/mycompany/javaoop/LinkedList.java
+++ JavaOOP/src/main/java/com/mycompany/javaoop/LinkedList.java
@@ -12,4 +12,23 @@ package com.mycompany.javaoop;
 public class LinkedList {
     private static int counter; 
     private Node head;
+
+    public LinkedList() {
+    }
+    
+    // add element to a list
+    public void add(Object data){
+        if(head == null){
+            head = new Node(data);// head is the first node
+        }
+        // head is not null, traverse the list
+        Node temNode = new Node(data);
+        Node currentNode = head;
+        if(currentNode != null){
+            while(currentNode.getNextNode() != null){
+                currentNode = currentNode.getNextNode();
+            }
+            currentNode.setNextNode(temNode);
+        }
+    }
 }
